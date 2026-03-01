c2f454b3ec7c880f89c8ff62572071ffc9e5887e
Jason Delgado <delgadorjason7@gmail.com>
2026-02-28 22:04:49 -0500

    setup: init commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,26 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 import authRouter from "./routes/authRoutes.js";
 import meRouter from "./routes/meRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 import profileRouter from "./routes/profileRoutes.js";
+import truckRouter from "./routes/truckRoutes.js";
 
 const app = express();
 
+// ---- MIDDLEWARE ----
+app.use(cors());
 app.use(express.json());
+
+// ---- ROUTES ----
 
 app.use("/auth", authRouter);
 app.use("/", meRouter);
 app.use("/users", userRouter);
 app.use("/profiles", profileRouter);
+app.use("/trucks", truckRouter);
 
 app.get("/", (req, res) => {
   res.send("Home Page");
