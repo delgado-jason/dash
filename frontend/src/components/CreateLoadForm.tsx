9c9aa55a6c22ffbe44ce1cb27f6af80f983f5343
Jason Delgado <delgadorjason7@gmail.com>
2026-04-20 23:05:01 -0400

    feat: build CreateLoadForm UI with all sections - refs #42

--- frontend/src/components/CreateLoadForm.tsx
+++ frontend/src/components/CreateLoadForm.tsx
@@ -4,6 +4,27 @@ import type { Agent } from "@/types/agent";
 import type { Market } from "@/types/market";
 import type { CreateLoadInput } from "@/types/createLoadInput";
 
+// UI Component Imports
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 // Interface for the createLoadForm props
 interface CreateLoadFormProps {
   brokers: Broker[];
@@ -43,7 +64,254 @@ const CreateLoadForm = ({
     loaded_miles: null,
     payment_status: "unpaid",
   });
-  return <></>;
+
+  // Filters agents to the broker
+  const filteredAgents = agents.filter(
+    (agent) => agent.broker_id === formData.broker_id,
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Open Dialog</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <div>
+          <DialogHeader>
+            <DialogTitle>Create Load</DialogTitle>
+            <DialogDescription className="sr-only">
+              Create a new load
+            </DialogDescription>
+          </DialogHeader>
+          <div className="mt-4">
+            <h3 className="text-sm font-semibold mb-2">Identification</h3>
+            <div className="grid grid-cols-2 gap-3">
+              <Label htmlFor="load_number">Load Number</Label>
+              <Input
+                name="load_number"
+                id="load_number"
+                onChange={handleChange}
+              ></Input>
+
+              <Select
+                onValueChange={(value) =>
+                  setFormData({
+                    ...formData,
+                    broker_id: value,
+                    agent_id: "",
+                  })
+                }
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Select a broker" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {brokers.map((broker) => (
+                      <SelectItem
+                        key={broker.broker_id}
+                        value={broker.broker_id}
+                      >
+                        {broker.broker_name}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+
+              <Select
+                onValueChange={(value) =>
+                  setFormData({
+                    ...formData,
+                    agent_id: value,
+                  })
+                }
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Select an agent" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {filteredAgents.map((agent) => (
+                      <SelectItem key={agent.agent_id} value={agent.agent_id}>
+                        {agent.first_name + " " + agent.last_name}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+
+              <Select
+                onValueChange={(value) =>
+                  setFormData({
+                    ...formData,
+                    load_type: value,
+                  })
+                }
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Select the load type" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectItem value="standard flatbed">
+                      Standard Flatbed
+                    </SelectItem>
+                    <SelectItem value="oversize">Oversize</SelectItem>
+                    <SelectItem value="heavy haul">Heavy Haul</SelectItem>
+                    <SelectItem value="hazmat">Hazmat</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+
+          <div className="mt-4">
+            <h3 className="text-sm font-semibold mb-2">When</h3>
+            <div className="grid grid-cols-2 gap-3">
+              <Label htmlFor="pickup_date">Pickup Date</Label>
+              <Input
+                type="date"
+                name="pickup_date"
+                id="pickup_date"
+                onChange={handleChange}
+              ></Input>
+              <Label htmlFor="delivery_date">Pickup Date</Label>
+              <Input
+                type="date"
+                name="delivery_date"
+                id="delivery_date"
+                onChange={handleChange}
+              ></Input>
+            </div>
+          </div>
+
+          <div className="mt-4">
+            <h3 className="text-sm font-semibold mb-2">Where</h3>
+            <div className="grid grid-cols-2 gap-3">
+              <Label htmlFor="origin_city">Origin City</Label>
+              <Input
+                name="origin_city"
+                id="origin_city"
+                onChange={handleChange}
+              ></Input>
+              <Label htmlFor="origin_state">Origin State</Label>
+              <Input
+                name="origin_state"
+                id="origin_state"
+                onChange={handleChange}
+              ></Input>
+              <Select
+                onValueChange={(value) =>
+                  setFormData({
+                    ...formData,
+                    origin_market_id: value,
+                  })
+                }
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Origin market" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {markets.map((market) => (
+                      <SelectItem
+                        key={market.market_id}
+                        value={market.market_id}
+                      >
+                        {market.market_name}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Label htmlFor="destination_city">Destination City</Label>
+              <Input
+                name="destination_city"
+                id="destination_city"
+                onChange={handleChange}
+              ></Input>
+              <Label htmlFor="destination_state">Destination State</Label>
+              <Input
+                name="destination_state"
+                id="destination_state"
+                onChange={handleChange}
+              ></Input>
+              <Select
+                onValueChange={(value) =>
+                  setFormData({
+                    ...formData,
+                    destination_market_id: value,
+                  })
+                }
+              >
+                <SelectTrigger>
+                  <SelectValue placeholder="Destination market" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    {markets.map((market) => (
+                      <SelectItem
+                        key={market.market_id}
+                        value={market.market_id}
+                      >
+                        {market.market_name}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+            </div>
+          </div>
+
+          <div className="mt-4">
+            <h3 className="text-sm font-semibold mb-2">What</h3>
+            <div className="grid grid-cols-2 gap-3">
+              <Label htmlFor="shipper_name">Shipper Name</Label>
+              <Input
+                name="shipper_name"
+                id="shipper_name"
+                onChange={handleChange}
+              ></Input>
+              <Label htmlFor="receiver_name">Receiver Name</Label>
+              <Input
+                name="receiver_name"
+                id="receiver_name"
+                onChange={handleChange}
+              ></Input>
+              <Label htmlFor="commodity">Commodity</Label>
+              <Input
+                name="commodity"
+                id="commodity"
+                onChange={handleChange}
+              ></Input>
+              <Label htmlFor="weight">Weight</Label>
+              <Input
+                type="number"
+                name="weight"
+                id="weight"
+                onChange={handleChange}
+              ></Input>
+              <Label htmlFor="dimensions">Dimensions</Label>
+              <Input
+                name="dimensions"
+                id="dimensions"
+                placeholder="8.5' x 13.5' x 73'"
+                onChange={handleChange}
+              ></Input>
+            </div>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default CreateLoadForm;
--- frontend/src/pages/LoadsPage.tsx
+++ frontend/src/pages/LoadsPage.tsx
@@ -1,4 +1,7 @@
 import { useLoads } from "@/hooks/useLoads";
+import { useBrokers } from "@/hooks/useBrokers";
+import { useAgents } from "@/hooks/useAgents";
+import { useMarkets } from "@/hooks/useMarkets";
 import {
   Table,
   TableBody,
@@ -9,9 +12,14 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import CreateLoadForm from "../components/CreateLoadForm";
 
 const LoadsPage = () => {
   const { loads, isLoading, error } = useLoads();
+  const { brokers } = useBrokers();
+  const { agents } = useAgents();
+  const { markets } = useMarkets();
+
   console.log(loads);
 
   if (isLoading) {
@@ -30,47 +38,59 @@ const LoadsPage = () => {
     );
   }
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Load #</TableHead>
-          <TableHead>Status</TableHead>
-          <TableHead>Broker</TableHead>
-          <TableHead>Origin</TableHead>
-          <TableHead>Destination</TableHead>
-          <TableHead>Pickup Date</TableHead>
-          <TableHead>Linehaul</TableHead>
-          <TableHead>Payment Status</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {loads.map((load) => (
-          <TableRow key={load.load_id}>
-            <TableCell>{load.load_number}</TableCell>
-            <TableCell>{load.load_status}</TableCell>
-            <TableCell>{load.broker}</TableCell>
-            <TableCell>{load.origin_city + ", " + load.origin_state}</TableCell>
-            <TableCell>
-              {load.destination_city + ", " + load.destination_state}
-            </TableCell>
-            <TableCell>
-              {new Date(load.pickup_date).toLocaleDateString("en-US", {
-                month: "short",
-                day: "numeric",
-                year: "numeric",
-              })}
-            </TableCell>
-            <TableCell>
-              {Number(load.linehaul).toLocaleString("en-US", {
-                style: "currency",
-                currency: "USD",
-              })}
-            </TableCell>
-            <TableCell>{load.payment_status}</TableCell>
+    <div>
+      <div>
+        <CreateLoadForm
+          brokers={brokers}
+          agents={agents}
+          markets={markets}
+          onSuccess={() => null}
+        />
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Load #</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Broker</TableHead>
+            <TableHead>Origin</TableHead>
+            <TableHead>Destination</TableHead>
+            <TableHead>Pickup Date</TableHead>
+            <TableHead>Linehaul</TableHead>
+            <TableHead>Payment Status</TableHead>
           </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+        </TableHeader>
+        <TableBody>
+          {loads.map((load) => (
+            <TableRow key={load.load_id}>
+              <TableCell>{load.load_number}</TableCell>
+              <TableCell>{load.load_status}</TableCell>
+              <TableCell>{load.broker}</TableCell>
+              <TableCell>
+                {load.origin_city + ", " + load.origin_state}
+              </TableCell>
+              <TableCell>
+                {load.destination_city + ", " + load.destination_state}
+              </TableCell>
+              <TableCell>
+                {new Date(load.pickup_date).toLocaleDateString("en-US", {
+                  month: "short",
+                  day: "numeric",
+                  year: "numeric",
+                })}
+              </TableCell>
+              <TableCell>
+                {Number(load.linehaul).toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "USD",
+                })}
+              </TableCell>
+              <TableCell>{load.payment_status}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 };
 
