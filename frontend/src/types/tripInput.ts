1eaffdc5700f2abd3dae24f34ec5f74c82964d45
Jason Delgado <delgadorjason7@gmail.com>
2026-07-07 11:38:47 -0700

    feat: add trips purpose to the trips data and make it mandatory

--- frontend/src/types/tripInput.ts
+++ frontend/src/types/tripInput.ts
@@ -2,6 +2,7 @@ export interface TripInput {
   truck_id?: string;
   driver_id?: string;
   trip_date: string;
+  trip_purpose: "repositioning" | "home" | "shop" | "personal";
   odometer_start?: number;
   odometer_end?: number;
   is_estimated?: boolean;
